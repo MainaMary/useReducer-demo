59d4264c2ff8ec6b0b535fbbeb9a561b15aad5fc
Mary <marieshix98@gmail.com>
2023-01-18 21:00:08 +0300

    Add cart page navigation link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { useShoppingCart } from '../context/appContext'
 
 const Navbar = () => {
+  const {state} = useShoppingCart()
   return (
     <div className='px-16 md:px-32 md:flex justify-between  w-full h-16 items-center'>
         <p>Shopping Spree</p>
+        <Link to="/cart">
         <p>Cart</p>
+        <p>{state.cart.length}</p>
+        </Link>
+        
     </div>
   )
 }
--- src/reducers/productReducer.ts
+++ src/reducers/productReducer.ts
@@ -1,20 +1,18 @@
-import { ProductsProps , ActionTypes, initialStateType} from "../model/types"
+import { ProductsProps, ActionTypes, initialStateType } from "../model/types"
 
-interface Props {
-    type: string;
-    payload : string | ProductsProps
-}
-export const productReducer = (state:initialStateType , action:any) =>{
-const {type, payload} = action
-switch(type){
-    case ActionTypes.Add:
-        const cartItems = [...state.cart, {quantity:1, ...payload}]
-        console.log({...state, cart:cartItems},'cart' )
-        return {...state, cart:cartItems}
-    case ActionTypes.Delete:
-        const filteredItems = state.cart.filter(item =>item.id !==payload.id )
-        return {...state, cart:filteredItems }
-    default: 
-    return state
-}
+
+export const productReducer = (state: initialStateType, action: any) => {
+    const { type, payload } = action
+
+    switch (type) {
+        case ActionTypes.Add:
+            const itemIndex = state.cart.findIndex(item => item.id === payload.id)
+            const cartItems = [...state.cart, { cartQuantity: 1, ...payload }]
+            return { ...state, cart: cartItems }
+        case ActionTypes.Delete:
+            const filteredItems = state.cart.filter(item => item.id !== payload.id)
+            return { ...state, cart: state.cart.filter(item => item.id !== payload.id) }
+        default:
+            return state
+    }
 }
