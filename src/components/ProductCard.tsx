5bc94526d05d6a7069a6e15a58e5d27688727e4f
Mary <marieshix98@gmail.com>
2023-01-18 21:01:05 +0300

    Add functionality to add items to the cart

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React ,{useState, useEffect} from 'react'
 import { ProductsProps, ActionTypes } from '../model/types';
 import { useShoppingCart } from '../context/appContext';
 
@@ -6,23 +6,42 @@ interface SingleProduct {
     product: ProductsProps
 }
 const ProductCard = ({ product }: SingleProduct) => {
-    const { name, image, price, delivery } = product
+    const [isInCart, setIsInCart] = useState(false)
+    const { name, image, price, delivery ,id} = product
     const { state, dispatch } = useShoppingCart()
     const { cart } = state
     const handleAddToCart = (product: ProductsProps) => {
         dispatch({ type: ActionTypes.Add, payload: product })
     }
-    console.log(state)
+    const handleRemoveFromCart = (productId:string)=>{
+        console.log(productId)
+        dispatch({ type: ActionTypes.Delete, payload: productId })
+    }
+    useEffect(()=>{
+        const productIsInCart = cart.find(product =>product.id === id)
+        if(productIsInCart){
+            setIsInCart(true)
+        }else{
+            setIsInCart(false)
+        }
+
+    },[cart, id])
     return (
         <div className="shadow-lg rounded-sm px-2 md:px-8 py-4">
             <img src={image} alt={name} />
             <p>{name}</p>
             <p>{price.split('.')[0]}</p>
             <div>{delivery ? <h2>Fast delivery</h2> : <h2>3 days delivery</h2>}</div>
+           
             <div>
-                {cart.length && cart.some(prod => prod.id !== product.id ? <button onClick={() => handleAddToCart(product)}>Add to cart</button>
-                    : <button>Remove from cart</button>)}
+                { 
+                isInCart
+                ? <button onClick={() =>handleRemoveFromCart(product.id)}>Remove from cart</button> :
+                <button onClick={() => handleAddToCart(product)}>Add to cart</button>
+                
+                }       
             </div>
+
         </div>
 
 
--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,8 +1,53 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { Link } from 'react-router-dom'
+import { useShoppingCart } from '../context/appContext'
+import { ProductsProps } from '../model/types'
+import { ActionTypes } from '../model/types'
 
+interface CartProps {
+  cartItem: ProductsProps
+}
+export const CartCard = ({cartItem}:CartProps) =>{
+  const [count, setCount] =useState(1)
+  const { name, image, price, delivery , id} = cartItem
+  const {dispatch} = useShoppingCart()
+  const removeItem = (id:string) =>{
+    dispatch({
+      type: ActionTypes.Delete,
+      payload: id
+    })
+  }
+  return <div className='shadow-lg rounded-md w-full'>
+  <div className='grid grid-cols-1 md:grid-cols-5'>
+    <img src={image} alt={name}/>
+    <p>{name}</p>
+    <p>{price}</p>
+    <p>{delivery}</p>
+    <div className='grid grid-cols-3 '>
+      <p>+</p>
+      <p>{count}</p>
+      <p>-</p>
+    </div>
+    <></>
+
+  </div>
+  <div>
+    <button onClick={() => removeItem(id)}>Remove from cart</button>
+  </div>
+  </div>
+  
+}
 const Cart = () => {
+  const {state:{cart}, dispatch} = useShoppingCart()
   return (
-    <div>Cart</div>
+    <div className='flex gap-3'>
+      <div className='w-[80%]'>
+        {!cart.length ? <p>Cart is empty. <Link to="/">Add items</Link></p>:cart.map(item =><CartCard key={item.id} cartItem={item}/>)}
+      </div>
+      <div className='w-[20%]'>
+        <p>Sub total</p>
+      </div>
+    </div>
   )
 }
 
