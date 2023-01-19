5b78bbd61590dd090a240784d0f1de79243936c9
Mary <marieshix98@gmail.com>
2023-01-19 13:18:22 +0300

    Update products list based on the filter param

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,11 +1,34 @@
 import React from 'react'
 import ProductCard from '../components/ProductCard'
 import { useShoppingCart } from '../context/appContext'
+import Filters from '../components/Filters'
+import { useEffect } from 'react'
 const Homepage = () => {
-  const {state:{products}}= useShoppingCart()
+  const { state: { products }, filters:{searchParam, fastDelivery} } = useShoppingCart()
+  let productList = products
+  useEffect(()=>{
+    
+  },[])
+  const updatedProducts =()=>{
    
+    if(searchParam){
+     productList = products.filter(label =>label.name.toLowerCase().includes(searchParam))
+    }
+    if(fastDelivery){
+      productList = productList.filter(item =>item.delivery)
+    }
+    
+    return productList
+  }
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-3 '>{products.map(product=><ProductCard key={product.id} {...product} product={product}/>)}</div>
+    <>
+    <Filters/>
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-4 mt-3 '>
+        {updatedProducts().map(product => <ProductCard key={product.id} {...product} product={product} />)}
+      </div>
+    </>
+
   )
 }
 
